62cedee4e588cd63faafc1ccc11de12479af7264
saketh-crystalwg <saketh@crystalwg.com>
2023-10-07 23:15:49 +0530

    first push

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python my_aff_comission.py
---
+++ my_aff_comission.py
@@ -0,0 +1,22 @@
+import json
+import pandas as pd
+import datetime
+import  requests
+from requests.auth import HTTPBasicAuth
+import xmltodict
+
+
+r = requests.get('https://admin.crystalwgpartners.com/feeds.php?FEED_ID=25&FROM_DATE=2023-09-01&TO_DATE=2023-09-07',\
+                auth = HTTPBasicAuth('SakethGV', 'CWGfg2023!'))
+
+data_dict = xmltodict.parse(r.text)
+
+json_data = json.dumps(data_dict)
+
+df = json.loads(json_data)
+
+df = pd.DataFrame(df)
+
+df_1 = pd.json_normalize(df['EARNINGS']['USER'])
+
+df_1
